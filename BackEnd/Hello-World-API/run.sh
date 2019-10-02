5af3417b2dcdbd4b0704705dbe31d0791cd1b886
Guruprasad Bhavsar <guruprasad.bhavsar@gmail.com>
2019-10-02 14:04:41 +0100

    replaced sh with java

--- BackEnd/Hello-World-API/run.sh
+++ BackEnd/Hello-World-API/run.sh
@@ -5,12 +5,12 @@ then
     if [ -f /application.jar/*.jar ];
     then
         echo "Running /application.jar/*.jar..."
-        sh /application.jar/*.jar
+        java -jar /application.jar/*.jar
     else
         echo "Application Jar not Found...Exiting..."
         exit 1
      fi
 else
     echo "Running /application.jar..."
-    sh /application.jar
+    java -jar /application.jar
 fi
